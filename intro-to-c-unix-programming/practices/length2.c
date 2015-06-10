7a60dac28c60c4f9b8e48646db64716c482e318c
katzhang-ias <arcadychang@gmail.com>
2015-06-09 22:34:06 -0400

    added length, length2, reverse and repeat digit

---
+++ intro-to-c-unix-programming/practices/length2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int len = 0;
+
+	printf("enter a message: ");
+	while (getchar() != '\n') {
+		len++;
+	}
+
+	printf("your message was %d character(s) long.\n", len);
+
+	return 0;
+}
