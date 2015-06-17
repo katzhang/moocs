93810b0ab72e95c07201a74dabb452bf4110eb4e
katzhang-ias <arcadychang@gmail.com>
2015-06-16 22:55:08 -0400

    added reverse3

---
+++ intro-to-c-unix-programming/practices/reverse3.c
@@ -0,0 +1,18 @@
+/* Reverses a series of numbers (pointer version) */
+#include <stdio.h>
+#define N 5
+
+int main(void) {
+	int a[N], *p;
+
+	printf("enter %d numbers: ", N);
+	for (p = a; p < a + N; p++)
+		scanf("%d", p);
+
+	printf("in reverse order: ");
+	for (p = a + N - 1; p >= a; p--) 
+		printf(" %d", *p);
+	printf("\n");
+
+	return 0;
+}
