fef5603151b8c0fa7893d65331183901f2c83ff0
katzhang-ias <arcadychang@gmail.com>
2015-07-06 22:25:50 -0400

    added file io stream

---
+++ google-c++-course/getting-started/file.cpp
@@ -0,0 +1,34 @@
+// file.cpp, Maggie Johnson
+// Description: An illustration of file processing
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main() {
+  char first_name[30], last_name[30]; int age;
+  char file_name[20];
+  // Collect the data.
+  cout << "Enter First Name: "; cin >> first_name;
+  cout << "Enter Last Name: "; cin >> last_name;
+  cout << "Enter Age: "; cin >> age;
+  cout << endl << "Enter the name of the file: "; cin >> file_name;
+
+  // Create an ofstream called People, open the stream for output.
+  ofstream People(file_name, ios::app);
+  // Write the output to the stream.
+  People << first_name << endl << last_name << endl << age << endl;
+
+  People.close();
+
+  ifstream People_in(file_name, ios::in);
+
+  cout << endl << "First Name: " << first_name;
+  cout << endl << "Last Name:  " << last_name;
+  cout << endl << "Enter Age:  " << age;
+  cout << endl;
+
+  // Close the stream.
+  People_in.close();
+
+  return 0;
+}
