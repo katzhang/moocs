9be4936a0e2a250dac974f24268e08ebe37a2423
katzhang-ias <arcadychang@gmail.com>
2015-07-28 21:09:19 -0400

    added templates

--- full-stack-foundations/restaurantmenu/project.py
+++ full-stack-foundations/restaurantmenu/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -12,24 +12,22 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurantmenu(restaurant_id):
+def restaurantMenu(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-	output = ''
-	for i in items:
-		output += i.name
-		output += '<br>'
-		output += i.price
-		output += '<br>'
-		output += i.description
-		output += '<br><br>'
-	return output
+	return render_template('menu.html', restaurant = restaurant, items = items)
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/new')
+@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+	if (request.method == 'POST'):
+		newItem = MenuItem(name=request.form['name'], description=request.form['description'], price=request.form['price'], restaurant_id=restaurant_id)
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
