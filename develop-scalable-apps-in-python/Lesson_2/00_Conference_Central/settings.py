d6ba6e052e5c7b0fc0829d7a6e283c2c3a6cd0df
katzhang-ias <arcadychang@gmail.com>
2015-08-11 00:00:45 -0400

    moved files to be under direct course folder

---
+++ develop-scalable-apps-in-python/Lesson_2/00_Conference_Central/settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '569901591726-vvap5l642f25ia3drtpus4h7omvv6te6.apps.googleusercontent.com'
+
