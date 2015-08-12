62c092f87e7ba7dd393e00e38ddee9e14fdf1ce6
katzhang-ias <arcadychang@gmail.com>
2015-08-11 22:46:11 -0400

    added datastore key to profile

--- develop-scalable-apps-in-python/Lesson_3/00_Conference_Central/conference.py
+++ develop-scalable-apps-in-python/Lesson_3/00_Conference_Central/conference.py
@@ -28,6 +28,7 @@ from models import ProfileForm
 from models import TeeShirtSize
 
 from settings import WEB_CLIENT_ID
+from utils import getUserId
 
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
@@ -70,19 +71,23 @@ class ConferenceApi(remote.Service):
         #         and import getUserId from it
         # step 2. get user id by calling getUserId(user)
         # step 3. create a new key of kind Profile from the id
+        user_id = getUserId(user)
+        p_key = ndb.Key(Profile, user_id)
 
         # TODO 3
         # get the entity from datastore by using get() on the key
+
         profile = None
         if not profile:
             profile = Profile(
-                key = None, # TODO 1 step 4. replace with the key from step 3
+                key = p_key, # TODO 1 step 4. replace with the key from step 3
                 displayName = user.nickname(), 
                 mainEmail= user.email(),
                 teeShirtSize = str(TeeShirtSize.NOT_SPECIFIED),
             )
             # TODO 2
             # save the profile to datastore
+            profile.put()
 
         return profile      # return Profile
 
--- develop-scalable-apps-in-python/Lesson_3/00_Conference_Central/settings.py
+++ develop-scalable-apps-in-python/Lesson_3/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '569901591726-vvap5l642f25ia3drtpus4h7omvv6te6.apps.googleusercontent.com'
 
